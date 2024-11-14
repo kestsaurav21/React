8b42b552ae3a4de5c94f954a8ef813fa7d92879e
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-14 15:18:24 +0530

    update Notes

--- Basics/app.js
+++ Basics/app.js
@@ -1,13 +1,35 @@
-const heading = React.createElement( //this method create a React element
-    "h1", 
-    {id: "heading", className: "header" },
-    "We are learning basics of React."
+/* 
+    <div id="parent">
+        <div id="child">
+            <h1></h1>
+        </div>
+    </div> 
+* ReactELement(Object) => HTML(Browser Understand)    
+*/
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement(
+        "div",
+        {id: "child"},
+        React.createElement(
+            "h1",
+            {},
+            "Hello, This is H1 heading."
+        )
+    )
 )
 
+const heading = React.createElement(
+  //this method create a React element
+  "h1",
+  { id: "heading", className: "header" },
+  "We are learning basics of React."
+);
 
 console.log(heading); //return a Object (React Element)
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);  //render() : takes the object then convert it into HTML element pass to the DOM
+root.render(heading); //render() : takes the object then convert it into HTML element pass to the DOM
